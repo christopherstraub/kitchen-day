5486f101df8568b26c724a77a6986105435996ba
Christopher Straub <ccstraub@gmail.com>
2021-02-18 20:18:53 -0600

    date, time, and countdown now update live. refactored code. changed styles.

--- script.js
+++ script.js
@@ -1,15 +1,41 @@
+// Variable initializations.
+var dateElement = document.querySelector('.date');
+var timeElement = document.querySelector('.time');
+var countdownElement = document.querySelector('.countdown');
+
+var people = [
+  { index: 0, name: 'Christopher' },
+  { index: 1, name: 'not Christopher' },
+];
+
+const constraint = {
+  personIndex: people[0].index,
+  lastTurnDate: new Date(Date.UTC(2021, 0, 10, 6, 0, 0)),
+};
+
 // Functions declarations.
 function getDaysSinceEpoch(date) {
   return date / 8.64e7;
 }
 
-function setCurrentTurn(people, fullDaysSinceLastTurn) {
+function setCurrentTurn(people, date) {
+  var fullDaysSinceLastTurn = Math.floor(
+    getDaysSinceEpoch(date) - getDaysSinceEpoch(constraint.lastTurnDate)
+  );
+
   if (fullDaysSinceLastTurn % 2 === 0)
     document.getElementsByClassName('turn')[0].textContent = people[0].name;
   else document.getElementsByClassName('turn')[0].textContent = people[1].name;
 }
 
-function getTimeUntilNextTurn(daysSinceLastTurn, fullDaysSinceLastTurn) {
+function getTimeUntilNextTurn(date) {
+  var daysSinceLastTurn =
+    getDaysSinceEpoch(date) - getDaysSinceEpoch(constraint.lastTurnDate);
+
+  var fullDaysSinceLastTurn = Math.floor(
+    getDaysSinceEpoch(date) - getDaysSinceEpoch(constraint.lastTurnDate)
+  );
+
   var daysUntilNextTurn = 1 - (daysSinceLastTurn - fullDaysSinceLastTurn);
 
   var hoursUntilNextTurn = daysUntilNextTurn * 24;
@@ -27,58 +53,49 @@ function getTimeUntilNextTurn(daysSinceLastTurn, fullDaysSinceLastTurn) {
   ];
 }
 
-// Variable initializations.
-var date = new Date();
+function getPaddedTimeUntilNextTurn(timeUntilNextTurn) {
+  var flooredTimeUntilNextTurn = timeUntilNextTurn.map(Math.floor);
+  return flooredTimeUntilNextTurn.map(
+    (value) => (value < 10 ? '0' : '') + value
+  );
+}
 
-var people = [
-  { index: 0, name: 'Christopher' },
-  { index: 1, name: 'not Christopher' },
-];
+function setDate(dateElement) {
+  dateElement.textContent = new Intl.DateTimeFormat('en-US', {
+    timeZone: Intl.DateTimeFormat().resolvedOptions().timeZone,
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  }).format(date);
+}
 
-const constraint = {
-  personIndex: people[0].index,
-  lastTurnDate: new Date(Date.UTC(2021, 0, 10, 6, 0, 0)),
-};
+function setTime(timeElement) {
+  timeElement.textContent = new Intl.DateTimeFormat('en-US', {
+    timeZoneName: 'short',
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric',
+  }).format(date);
+}
 
-var daysSinceLastTurn =
-  getDaysSinceEpoch(date) - getDaysSinceEpoch(constraint.lastTurnDate);
+function setCountdown(countdownElement, paddedTimeUntilNextTurn) {
+  countdownElement.textContent = `${paddedTimeUntilNextTurn[0]}:${paddedTimeUntilNextTurn[1]}:${paddedTimeUntilNextTurn[2]}`;
+}
 
-var fullDaysSinceLastTurn = Math.floor(
-  getDaysSinceEpoch(date) - getDaysSinceEpoch(constraint.lastTurnDate)
-);
+function update() {
+  date = new Date();
 
-var timeUntilNextTurn = getTimeUntilNextTurn(
-  daysSinceLastTurn,
-  fullDaysSinceLastTurn
-);
+  setCurrentTurn(people, date);
+
+  var timeUntilNextTurn = getTimeUntilNextTurn(date);
+  var paddedTimeUntilNextTurn = getPaddedTimeUntilNextTurn(timeUntilNextTurn);
+
+  setDate(dateElement);
+  setTime(timeElement);
+  setCountdown(countdownElement, paddedTimeUntilNextTurn);
+}
 
 // Function calls.
-setCurrentTurn(people, fullDaysSinceLastTurn);
-
-// DOM manipulations.
-document.getElementsByClassName(
-  'date'
-)[0].textContent = new Intl.DateTimeFormat('en-US', {
-  timeZone: Intl.DateTimeFormat().resolvedOptions().timeZone,
-  weekday: 'long',
-  year: 'numeric',
-  month: 'long',
-  day: 'numeric',
-}).format(date);
-
-document.getElementsByClassName(
-  'time'
-)[0].textContent = new Intl.DateTimeFormat('en-US', {
-  timeZoneName: 'short',
-  hour: 'numeric',
-  minute: 'numeric',
-  second: 'numeric',
-}).format(date);
-
-document.getElementsByClassName(
-  'time-until-next-turn'
-)[0].textContent = `It will be the next person's turn in
-${Math.floor(timeUntilNextTurn[0])} hours, ${Math.floor(
-  timeUntilNextTurn[1]
-)} minutes, and ${Math.floor(timeUntilNextTurn[2])} seconds,
-`;
+update();
+setInterval(update, 1000);
