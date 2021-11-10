9975c57e093282bb345a845e8fe29ebf246f5435
Christopher Straub <ccstraub@gmail.com>
2021-11-09 21:42:09 -0600

    Add easter eggs

--- script.js
+++ script.js
@@ -1,70 +1,71 @@
 // Variable initializations.
-var turnElement = document.querySelector('.turn');
-var dateElement = document.querySelector('.date');
-var timeElement = document.querySelector('.time');
-var datePrefixElement = document.querySelector('.date-prefix');
-var timePrefixElement = document.querySelector('.time-prefix');
-var countdownElement = document.querySelector('.countdown');
-var previousDayElement = document.querySelector('.previous-day');
-var nextDayElement = document.querySelector('.next-day');
-var todayElement = document.querySelector('.today');
+let turnElement = document.querySelector('.turn');
+let dateElement = document.querySelector('.date');
+let timeElement = document.querySelector('.time');
+let datePrefixElement = document.querySelector('.date-prefix');
+let timePrefixElement = document.querySelector('.time-prefix');
+let countdownElement = document.querySelector('.countdown');
+let previousDayElement = document.querySelector('.previous-day');
+let nextDayElement = document.querySelector('.next-day');
+let todayElement = document.querySelector('.today');
+let easterEggElement = document.querySelector('.easter-egg');
 
-var people = [
+let people = [
   { index: 0, name: 'Christopher' },
   { index: 1, name: 'not Christopher' },
 ];
 
-var lastTurn = {
+let lastTurn = {
   personIndex: people[1].index,
   date: new Date(Date.UTC(2021, 2, 15, 6)),
 };
 
-var dateOffset = 0;
+let dayOffset = 0;
 
 // Functions declarations.
 
 // Check whether a date is observing daylight saving time.
-function isDST(date) {
+const isDST = (date) => {
   let jan = new Date(date.getFullYear(), 0, 1).getTimezoneOffset();
   let jul = new Date(date.getFullYear(), 6, 1).getTimezoneOffset();
   return Math.max(jan, jul) !== date.getTimezoneOffset();
-}
+};
 
-function getTurn(dateChecked, lastTurn) {
-  var fullDaysSinceLastTurn = Math.floor(
+const getTurn = (dateChecked, lastTurn) => {
+  let fullDaysSinceLastTurn = Math.floor(
     Math.floor((dateChecked - lastTurn.date) / 8.64e7)
   );
 
   if (fullDaysSinceLastTurn % 2 === 0) return people[lastTurn.personIndex].name;
   else if (lastTurn.personIndex === 0) return people[1].name;
   else return people[0].name;
-}
+};
 
-function setTurn(turn) {
+const setTurn = (turn) => {
   turnElement.textContent =
-    dateOffset < 0
+    dayOffset < 0
       ? `
   It was ${turn}'s turn this day.`
-      : dateOffset > 0
+      : dayOffset > 0
       ? `
   It's ${turn}'s turn this day.`
       : `It's ${turn}'s turn today.`;
-}
+};
 
-function getCountdown(dateChecked, lastTurn) {
+const getCountdown = (dateChecked, lastTurn) => {
   if (isDST(dateChecked)) dateChecked.setHours(dateChecked.getHours() + 1);
 
-  var millisecondsSinceLastTurn = dateChecked - lastTurn.date;
+  let millisecondsSinceLastTurn = dateChecked - lastTurn.date;
 
-  var millisecondsUntilNextTurn =
+  let millisecondsUntilNextTurn =
     Math.ceil(millisecondsSinceLastTurn / 8.64e7) * 8.64e7 -
     millisecondsSinceLastTurn;
 
-  var fullHoursUntilNextTurn = Math.floor(millisecondsUntilNextTurn / 3.6e6);
-  var fullMinutesUntilNextTurn = Math.floor(
+  let fullHoursUntilNextTurn = Math.floor(millisecondsUntilNextTurn / 3.6e6);
+  let fullMinutesUntilNextTurn = Math.floor(
     (millisecondsUntilNextTurn - fullHoursUntilNextTurn * 3.6e6) / 6e4
   );
-  var fullSecondsUntilNextTurn = Math.floor(
+  let fullSecondsUntilNextTurn = Math.floor(
     (millisecondsUntilNextTurn -
       fullHoursUntilNextTurn * 3.6e6 -
       fullMinutesUntilNextTurn * 6e4) /
@@ -76,13 +77,12 @@ function getCountdown(dateChecked, lastTurn) {
     fullMinutesUntilNextTurn,
     fullSecondsUntilNextTurn,
   ];
-}
+};
 
-function getPaddedCountdown(countdown) {
-  return countdown.map((value) => (value < 10 ? '0' : '') + value);
-}
+const getPaddedCountdown = (countdown) =>
+  countdown.map((value) => (value < 10 ? '0' : '') + value);
 
-function setDate(dateElement, date) {
+const setDate = (dateElement, date) => {
   dateElement.textContent = new Intl.DateTimeFormat('en-US', {
     timeZone: Intl.DateTimeFormat().resolvedOptions().timeZone,
     weekday: 'long',
@@ -90,32 +90,42 @@ function setDate(dateElement, date) {
     month: 'long',
     day: 'numeric',
   }).format(date);
-}
+};
 
-function setTime(timeElement, date) {
+const setTime = (timeElement, date) => {
   timeElement.textContent = new Intl.DateTimeFormat('en-US', {
     timeZoneName: 'short',
     hour: 'numeric',
     minute: 'numeric',
     second: 'numeric',
   }).format(date);
-}
+};
 
-function setDatePrefix() {
-  datePrefixElement.textContent = !dateOffset ? 'Today is' : 'On';
-}
+const setDatePrefix = () => {
+  datePrefixElement.textContent = !dayOffset ? 'Today is' : 'On';
+};
 
-function setTimePrefix() {
-  timePrefixElement.textContent = !dateOffset ? 'The current time is' : 'At';
-}
+const setTimePrefix = () => {
+  timePrefixElement.textContent = !dayOffset ? 'The current time is' : 'At';
+};
 
-function setCountdown(countdownElement, paddedCountdown) {
+const setCountdown = (countdownElement, paddedCountdown) => {
   countdownElement.textContent = `${paddedCountdown[0]}:${paddedCountdown[1]}:${paddedCountdown[2]}`;
-}
+};
 
-function update() {
+const setEasterEgg = (month, day, year) => {
+  if (month === 12 && day === 7) {
+    easterEggElement.textContent = '🐕🎉🎂🎁';
+    easterEggElement.classList.remove('visually-hidden');
+  } else if (month === 12 && day === 25) {
+    easterEggElement.textContent = '🧔🎉🎂🎁';
+    easterEggElement.classList.remove('visually-hidden');
+  } else easterEggElement.classList.add('visually-hidden');
+};
+
+const update = () => {
   date = new Date();
-  date.setDate(date.getDate() + dateOffset);
+  date.setDate(date.getDate() + dayOffset);
   date.setMilliseconds(0);
 
   setTurn(getTurn(date, lastTurn));
@@ -126,13 +136,15 @@ function update() {
   setDatePrefix();
   setTimePrefix();
 
-  var paddedCountdown = getPaddedCountdown(getCountdown(date, lastTurn));
+  setEasterEgg(date.getMonth() + 1, date.getDate(), date.getFullYear());
+
+  let paddedCountdown = getPaddedCountdown(getCountdown(date, lastTurn));
 
   setCountdown(countdownElement, paddedCountdown);
 
-  if (dateOffset !== 0) todayElement.classList.remove('selected');
+  if (dayOffset !== 0) todayElement.classList.remove('selected');
   else todayElement.classList.add('selected');
-}
+};
 
 // Function calls.
 
@@ -140,16 +152,16 @@ update();
 setInterval(update, 1000);
 
 previousDayElement.addEventListener('click', () => {
-  dateOffset--;
+  dayOffset--;
   update();
 });
 
 nextDayElement.addEventListener('click', () => {
-  dateOffset++;
+  dayOffset++;
   update();
 });
 
 todayElement.addEventListener('click', () => {
-  dateOffset = 0;
+  dayOffset = 0;
   update();
 });
